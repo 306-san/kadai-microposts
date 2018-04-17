c1c992a47ddd6f2dd1d2c1126f593f6b39ce518a
306-san <oh.taku528+git@gmail.com>
2018-04-17 02:24:34 +0000

    user login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  include SessionsHelper
+
+  private
+
+  def require_user_logged_in
+    unless logged_in?
+      require_user_login_url
+    end
+  end
 end
